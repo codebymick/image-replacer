48813ed9e217c3f28828a81cf707a16c8b876cd9
Mick White <m.white@innovationmecom.de>
2019-03-12 12:04:35 +0100

    init commit

---
+++ 1.1.1/js/content.js
@@ -0,0 +1,69 @@
+ /**
+ * Created by PhpStorm.
+ * Author: m.white
+ * Date: 25.01.19
+ */
+
+var pr = {};
+pr.debug = false;
+pr.setting = {
+    class_name: 'pr-image-replace',
+    variation: 4
+}
+pr.image = {
+    change: function () {
+        pr.debug && console.log('image.change');
+
+        chrome.runtime.sendMessage({action: "pr_get_data"}, function (data) {
+            if (data.length) {
+                $('img:not([src *= "staticflickr.com"])')/*$("img:not('." + pr.setting.class_name + "')")*/.each(function () {
+                    if ($(this).width() / $(this).height() < pr.setting.variation && $(this).height() / $(this).width() < pr.setting.variation) {
+                        $(this).css({width: $(this).width(), height: $(this).height()})
+                            .data('pr-url-origin', this.src)
+                            .attr('src', data[Math.floor(Math.random() * data.length)])/*
+                            .addClass(pr.setting.class_name)*/;
+                    }
+                })
+            }
+        });
+        setTimeout(function () {
+            pr.image.change();
+        }, 2000);
+    },
+    retrieve: function () {
+        pr.debug && console.log('image.retrieve');
+
+        $('img[src *= "staticflickr.com"]')/*$('img.' + pr.setting.class_name)*/.each(function () {
+            $(this).attr('src', $(this).data('pr-url-origin'))/*.removeClass(pr.setting.class_name)*/;
+        });
+    }
+}
+
+chrome.runtime.onMessage.addListener(function (msg, sender, cb) {
+    pr.debug && console.log('chrome.runtime message', arguments);
+    if (msg.action == 'pr_change_power') {
+        if (msg.power) {
+            pr.image.change();
+        } else {
+            pr.image.retrieve();
+        }
+    }
+    if (msg.action == 'pr_check_data') {
+        if (msg.status == 'start') {
+            pr.image.retrieve();
+        } else if (msg.status == 'finish') {
+            pr.image.change();
+        } else if (msg.status == 'load') {
+            pr.image.retrieve();
+        }
+    }
+});
+
+chrome.runtime.sendMessage({action: "pr_get_power"}, function (power) {
+    pr.debug && console.log('chrome.runtime pr_get_power', power);
+    if (power) {
+        pr.image.change();
+    } else {
+        pr.image.retrieve();
+    }
+});
---
+++ 1.1.1/js/option.js
@@ -0,0 +1,48 @@
+ /**
+ * Created by PhpStorm.
+ * Author: m.white
+ * Date: 25.01.19
+ */
+
+$(document).ready(function () {
+
+    chrome.runtime.onMessage.addListener(function (msg, sender, cb) {
+        if (msg.action == 'pr_change_power') {
+            $('#change_status .btn').removeClass('btn-primary')
+            $('#change_status [data-power=' + msg.power + ']').addClass('btn-primary');
+        }
+        if (msg.action == 'pr_check_data') {
+            var $check_status = $('#check_status');
+            var $random_image = $('#random_image');
+            $check_status.slideDown();
+            if (msg.status == 'start') {
+                $check_status.text('Start checking images (please wait while checking)');
+            } else if (msg.status == 'finish') {
+                $check_status.text('Finish checking images').slideUp(1000);
+//                $random_image.slideDown(1000)
+//                    .find('img').attr('src', msg.check_data[Math.floor(Math.random() * msg.check_data.length)]);
+            } else if (msg.status == 'load') {
+                $check_status.text('Checking images: ' + (msg.index + 1) + ' of ' + msg.data.length + ' (please wait while checking)');
+                $random_image.slideDown(1000).find('img').attr('src', msg.check_data[msg.check_data.length - 1]);
+            }
+        }
+    });
+
+    chrome.runtime.sendMessage({action: "pr_get_power"}, function (power) {
+        $('#change_status .btn').removeClass('btn-primary');
+        $('#change_status [data-power=' + power + ']').addClass('btn-primary');
+    });
+
+    chrome.runtime.sendMessage({action: "pr_get_data"}, function (data) {
+        if (data.length) {
+            $('#random_image').slideDown().find('img').attr('src', data[Math.floor(Math.random() * data.length)]);
+        } else {
+            $('#check_status').slideDown().text('Start checking images (please wait while checking)');
+        }
+    });
+
+    $('#change_status .btn').on('click', function () {
+        chrome.runtime.sendMessage({action: "pr_set_power", power: $(this).data('power')});
+    })
+
+})
